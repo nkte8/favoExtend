093e31b0438f989e65e4f1eaa7bda842e3d4b055
nekono <91360587+nkte8@users.noreply.github.com>
2024-05-01 03:54:11 +0900

    ビルド工程が抜けていたため追加

--- modules/function/bundling.tf
+++ modules/function/bundling.tf
@@ -2,7 +2,7 @@
 data "external" "bundle" {
   program = [
     "bash", "-c",
-    "npx esbuild --bundle ${var.cloudflare_workers_script.entrypoint} --outfile=${var.cloudflare_workers_script.outfile} --format=esm --minify --sourcemap > /dev/null && echo \"{\\\"result\\\": \\\"ok\\\"}\""
+    "npm install >/dev/null && npx esbuild --bundle ${var.cloudflare_workers_script.entrypoint} --outfile=${var.cloudflare_workers_script.outfile} --format=esm --minify --sourcemap > /dev/null && echo \"{\\\"result\\\": \\\"ok\\\"}\""
   ]
   working_dir = "${path.root}/${var.cloudflare_workers_script.workdir}/"
 }
